d43512b12fd6830db1fdf74f81ab64bad6f9ac64
Ryosuke Takemoto <ryosuketakemoto@RyosukenoMacBook-Air.local>
2022-01-15 23:21:05 +0900

    second commit

--- login_register.py
+++ login_register.py
@@ -1,17 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#CSV 
+
+import csv 
+
+def buttonTestPushed():
+
+	userInfo='login_register for python practice_01152022.csv'
+	
+	with open(userInfo) as f:
+		reader=csv.reader(f)
+		infoList = [row for row in reader]
+
+	for x in range(len(infoList)):
+		if x >0:
+			lblDisplayName['text']=infoList[x][3]
+			lblDisplayAge['text']=infoList[x][4]
+			lblDisplayHeight['text']=infoList[x][5]
+
 import tkinter as tk
 
 #login function
+
 #login UI
 
 root=tk.Tk()
-root.geometry('600x600')
+root.geometry('800x800')
 root.title('Login_register')
 
 buttonLogin=tk.Button(root,text='login')
 buttonLogin.place(x=250,y=350)
+
+buttonDisplayTest=tk.Button(root,text='test',command=buttonTestPushed)
+buttonDisplayTest.place(x=250,y=450)
 
 lblEntryUserID=tk.Label(root,text='userID')
 lblEntryUserID.place(x=100,y=150)
@@ -25,27 +47,23 @@ lblEntryPass.place(x=100,y=250)
 txtboxEntryPass=tk.Entry(root,width=25)
 txtboxEntryPass.place(x=200,y=250)
 
-#labelDisplayError
-
 #labelDisplayInfo
-#labelDisplayUserName
-#labelDisplayUserAge
-#labelDisplayUserHeight
 
-#displayNumber=tk.Label(root,text='0',font=("MSゴシック", "40", "bold"),anchor=tk.W)
-#displayNumber.place(x=300,y=30)
+lblDisplayName=tk.Label(root,text='name')
+lblDisplayName.place(x=100, y=400)
+
+lblDisplayAge=tk.Label(root,text='age')
+lblDisplayAge.place(x=100, y=450)
+
+lblDisplayHeight=tk.Label(root,text='height')
+lblDisplayHeight.place(x=100, y=500)
+
+#labelDisplayError
 
 root.mainloop()
 
-#CSV 
 
-import csv 
 
-with open('login_register for python practice_01152022.csv') as f:
-	reader=csv.reader(f)
-	content = [row for row in reader]
-
-print(content)
 
 
 
