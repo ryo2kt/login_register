12976511140a4d41806b63fefd3a6869248185b7
Ryosuke Takemoto <ryosuketakemoto@RyosukenoMacBook-Air.local>
2022-01-18 00:04:54 +0900

    login only commit

--- login_register.py
+++ login_register.py
@@ -14,47 +14,30 @@ import csv
 import tkinter as tk
 import tkinter.ttk as ttk
 
-
 def buttonLoginPushed():
 	
 	userInfo='login_register for python practice_01152022.csv'	
+
 	txtboxEntryEmail_value=txtboxEntryEmail.get()
 	txtboxEntryPass_value=txtboxEntryPass.get()
 
 	with open(userInfo) as f:
-		reader=csv.reader(f)
-		infoList = [row for row in reader]
+		infoList=csv.reader(f)
 
-		for x in range(len(infoList)):
-			if txtboxEntryEmail_value == 'ryosuke@saciq.jp'\
-				and txtboxEntryPass_value == '333':
-				frame_logined.tkraise()
-				lblDisplayName['text']=infoList[3][3]
-				lblDisplayAge['text']=infoList[3][4]
-				lblDisplayHeight['text']=infoList[3][5]
+		emailInfo = txtboxEntryEmail_value
+		passInfo = txtboxEntryPass_value
 
-			elif txtboxEntryEmail_value == 'takahiro@saciq.jp'\
-				and txtboxEntryPass_value == '222':
-				frame_logined.tkraise()
-				lblDisplayName['text']=infoList[2][3]
-				lblDisplayAge['text']=infoList[2][4]
-				lblDisplayHeight['text']=infoList[2][5]
+		for user in infoList:
+			if user[1] == emailInfo:
+				if user[2] == passInfo:
+					frame_logined.tkraise()
+					lblDisplayName['text']=user[3]
+					lblDisplayAge['text']=user[4]
+					lblDisplayHeight['text']=user[5]
+					
+				else:
+					lblDisplayError['text']='error'
 
-			elif txtboxEntryEmail_value == 'shotaro@saciq.jp'\
-				and txtboxEntryPass_value == '111':
-				frame_logined.tkraise()
-				lblDisplayName['text']=infoList[1][3]
-				lblDisplayAge['text']=infoList[1][4]
-				lblDisplayHeight['text']=infoList[1][5]
-
-			else:
-				lblDisplayError['text']='error'
-
-#for x in range(len(infoList)):
-			#if x >0:
-				#lblDisplayName['text']=infoList[x][3]
-				#lblDisplayAge['text']=infoList[x][4]
-				#lblDisplayHeight['text']=infoList[x][5]
 
 def buttonLogoutPushed():
     
